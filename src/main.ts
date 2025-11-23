cd3e8f451883f88f29d60baf4343c63cae977103
Ali Foroughi <foroughi.ali@gmail.com>
2025-11-23 14:53:24 +0100

    added rotation in pwa

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,26 @@ import { getLevelScene } from "./util";
 import "./pwa";
 const lvl = getLevelScene();
 
-const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
-    backgroundColor: "#1e1e1e",
-    physics: {
-        default: "arcade",
-        arcade: {
-            gravity: { x: 0, y: 800 },
-            debug: false,
-        },
-    },
-    scene: [lvl],
-};
+window.addEventListener("load", () => {
+    const orientation: any = screen.orientation;
 
-new Phaser.Game(config);
+    if (orientation && orientation.lock) {
+        orientation.lock("portrait").catch(() => {});
+    }
+
+    const config: Phaser.Types.Core.GameConfig = {
+        type: Phaser.AUTO,
+        width: window.innerWidth,
+        height: window.innerHeight,
+        backgroundColor: "#1e1e1e",
+        physics: {
+            default: "arcade",
+            arcade: {
+                gravity: { x: 0, y: 800 },
+                debug: false,
+            },
+        },
+        scene: [lvl],
+    };
+    new Phaser.Game(config);
+});
