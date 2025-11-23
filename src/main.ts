04f21d6bd7af998453b50a8c8d43c52b36253ff5
Ali Foroughi <foroughi.ali@gmail.com>
2025-11-23 16:29:29 +0100

    trying to fix pwa orientation

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,40 @@ import { getLevelScene, isPWA } from "./util";
 import "./pwa";
 const lvl = getLevelScene();
 window.addEventListener("load", () => {
-    const config: Phaser.Types.Core.GameConfig = {
-        type: Phaser.AUTO,
-        width: window.innerWidth,
-        height: window.innerHeight,
-        backgroundColor: "#1e1e1e",
-        physics: {
-            default: "arcade",
-            arcade: {
-                gravity: { x: 0, y: 800 },
-                debug: false,
-            },
-        },
-        scene: [lvl],
-    };
-
     if (isPWA()) {
         const orientation: any = screen.orientation;
         orientation.lock("landscape").catch(() => {});
-        config.width = window.innerHeight;
-        config.height = window.innerWidth;
+
+        const config: Phaser.Types.Core.GameConfig = {
+            type: Phaser.AUTO,
+            width: window.innerHeight,
+            height: window.innerWidth,
+            backgroundColor: "#1e1e1e",
+            physics: {
+                default: "arcade",
+                arcade: {
+                    gravity: { x: 0, y: 800 },
+                    debug: false,
+                },
+            },
+            scene: [lvl],
+        };
+        new Phaser.Game(config);
+    } else {
+        const config: Phaser.Types.Core.GameConfig = {
+            type: Phaser.AUTO,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            backgroundColor: "#1e1e1e",
+            physics: {
+                default: "arcade",
+                arcade: {
+                    gravity: { x: 0, y: 800 },
+                    debug: false,
+                },
+            },
+            scene: [lvl],
+        };
+        new Phaser.Game(config);
     }
-    new Phaser.Game(config);
 });
